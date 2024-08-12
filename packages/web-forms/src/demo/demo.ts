604aa3c6fa3e89b5f92757993142d19d274774fc
Sadiq Khoja <sadiqkhoja@gmail.com>
2024-08-12 10:32:06 -0400

    Feature: add vue-router in Demo app

--- packages/web-forms/src/demo/demo.ts
+++ packages/web-forms/src/demo/demo.ts
@@ -1,15 +1,16 @@
 import type { Component } from 'vue';
 import { createApp } from 'vue';
 
+import { webFormsPlugin } from '../WebFormsPlugin';
 import OdkWebFormDemo from './OdkWebFormDemo.vue';
-import { webFormsPlugin } from './WebFormsPlugin';
 
-import icomoon from './assets/css/icomoon.css?inline';
-import theme from './themes/2024-light/theme.scss?inline';
+import icomoon from '../assets/css/icomoon.css?inline';
+import theme from '../themes/2024-light/theme.scss?inline';
 // TODO/sk: Purge it - postcss-purgecss
 import primeflex from 'primeflex/primeflex.css?inline';
 
-import demoStyles from './assets/css/style.scss?inline';
+import demoStyles from '../assets/css/style.scss?inline';
+import router from './router';
 
 const styles = [icomoon, theme, primeflex, demoStyles].join('\n\n');
 const stylesheet = new CSSStyleSheet();
@@ -20,4 +21,5 @@ document.adoptedStyleSheets.push(stylesheet);
 
 const app = createApp(OdkWebFormDemo as Component);
 app.use(webFormsPlugin);
+app.use(router);
 app.mount('#app');
